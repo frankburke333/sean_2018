f531c463d71326c7c4bf0c059b7dad2b951727b4
frankburke333 <frank.burke@gmail.com>
2017-09-12 17:22:10 -0400

    Sept newsletter

--- application.js
+++ application.js
@@ -1,17 +1,9 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
-//= require player_scoring_playoffs
-console.log('in application JS');
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector(".ottawa").addEventListener("click", function(event){
+  event.preventDefault();
+  $('.ottawa_update').show();
+  $('body').animate({scrollTop:$('.ottawa_update').offset().top},900)
+  }, false);
